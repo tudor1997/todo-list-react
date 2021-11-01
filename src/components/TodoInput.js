9d1e4937fdc0e8b9cca9279b13d7ce07a32c66c8
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-11-01 13:30:58 +0200

    TodoInput layout

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+        const {item, handleChange, handleSubmit, editItem} = this.props
         return (
-            <h1>
-              hello from todo input
-            </h1>
+         <div className="card card-body my-3">
+             <form onSubmit={handleSubmit}>
+                 <div className="input-group">
+                     <div className="input-group flex-nowrap">
+                         <div className="input-group-text bg-second text-black">
+                                <i className="fas fa-book"></i>    
+                         </div>
+                         <input 
+                            type="text" 
+                            className="form-control text-capitalize" 
+                            placeholder="add todo item" 
+                            value={item} onChange={handleChange} 
+                            />
+                     </div>
+                   
+                 </div>
+                 <div className="d-grid">
+                 <button type="submit" className="btn btn-outline-secondary mt-3">
+                     Add item
+                 </button>
+                 </div>
+               
+             </form>
+         </div>
         )
     }
 }
