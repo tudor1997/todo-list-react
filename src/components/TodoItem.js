998aacba87cce9081e453811569a5832d6ed637e
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-11-01 12:35:58 +0200

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <h1>
+                 hello from item
+            </h1>
+        )
+    }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Item from './TodoItem';
+
+
+export default class TodoList extends Component {
+    render() {
+        return (
+          <section>
+              <h2>Todo list</h2>
+              <Item></Item>
+          </section>
+        )
+    }
+}
